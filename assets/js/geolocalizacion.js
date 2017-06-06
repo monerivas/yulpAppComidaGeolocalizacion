9360fbaa7b499f4b55ca3f92c36207d3eb974a45
Mone Rivas <mone.rivas@gmail.com>
2017-06-06 11:53:24 -0500

    Salto a la version 4, mostrado de la ubicacion del restaurante en el mapa falta poner los objetos con DOM

--- assets/js/geolocalizacion.js
+++ assets/js/geolocalizacion.js
@@ -1,51 +1,53 @@
-/*var cargarPagina = function () {
-	$("#get-location").click(obtenerUbicacion);
-};*/
 
-var obtenerUbicacion = function (e) {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(mostrarPosicion);
-	} else {
-		alert("Actualice su navegador");
-	}
-};
-
-var mostrarPosicion = function (posicion) {
-	console.log(posicion);
-	// alert("Latitud: " + posicion.coords.latitude);
-	// alert("Longitud: " + posicion.coords.longitude);
-
-	var coordenadas = {
-		lat: posicion.coords.latitude, 
-		lng: posicion.coords.longitude
-	};
-	mostrarMapa(coordenadas);
-};
-
-var mostrarMapa = function (coordenadas) {
-	var map = new google.maps.Map($('.map')[0], {
-      zoom: 17,
-      center: coordenadas
-    });
-    var marker = new google.maps.Marker({
-      position: coordenadas,
-      map: map
-    });
+$(document).ready(cargarPagina);
+// HOISTING
+function cargarPagina() {
+  obtenerUbicacionActual();
+  $(".ciudad").click(cambiarUbicacion);
 }
 
-$(document).ready(obtenerUbicacion);
+function obtenerUbicacionActual() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(mostrarPosicionActual);
+  } else {
+    alert("Geolocalización no es soportado en tu navegador");
+  }
+}
 
-/*
-API KEY:   AIzaSyCvT5zFi6GQj0nhiQEVoHu1egn02RLjmjw
-*/
+function mostrarPosicionActual(posicion) {
+  var latitud = posicion.coords.latitude;
+  var longitud = posicion.coords.longitude;
 
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
 
+  mostrarMapa(coordenadas);
+}
 
+// @coordenadas: { lat: <number>, lng: <number> }
+function mostrarMapa(coordenadas) {
+  var map = new google.maps.Map($('.map')[0], {
+    zoom: 15,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
+}
 
+function cambiarUbicacion() {
+  var latitud = $(this).data("latitud");
+  var longitud = $(this).data("longitud");
+    console.log(this);
 
+  var coordenadas = {
+    lat: latitud,
+    lng: longitud
+  };
 
-
-
-
-
-
+  console.log(coordenadas);
+  mostrarMapa(coordenadas);
+}
